50bf3c155ed673f140e524ff143474ed4fb6264b
Rohit <rohit@nexthoughts.com>
2018-06-29 22:21:54 +0530

    React Demo

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+
+
+export const DB_CONFIG ={
+    apiKey: "AIzaSyCpATsx26gdH2M138MZiSsqU7Wn5D2rAEg",
+    authDomain: "demojune18-dadb6.firebaseapp.com",
+    databaseURL: "https://demojune18-dadb6.firebaseio.com",
+    projectId: "demojune18-dadb6",
+    storageBucket: "demojune18-dadb6.appspot.com",
+    messagingSenderId: "519856167715",
+};
